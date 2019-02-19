ec44ddd722740f03b12ba727dc366b1909742cec
taktyeMoti <hal.ishiitakahiro@gmail.com>
2019-02-19 15:03:43 +0900

    2017で復元完了

---
+++ Assets/Scripts/cameraMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class cameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private testPlayerController player;
+
+    [SerializeField]
+    private Transform start;
+    
+    [SerializeField]
+    private Transform goal;
+    
+    [SerializeField]
+    private int moveTime;
+
+	float length;
+	int a = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	length = 0;
+    	a = 0;
+        this.transform.position = new Vector3(start.position.x,start.position.y,start.position.z);
+
+		length = Vector3.Distance(start.position , goal.position);
+		length = length / ((float)moveTime*60);
+        //this.transform.DOMove(goal.position, moveTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(player.getDeth() == true)
+    	{
+    		if(a ==0)
+    		{    			
+    			a = 1;
+    		
+	    		Vector3 pos = new Vector3(this.transform.position.x , this.transform.position.y,this.transform.position.z + 5);
+		        this.transform.DOMove(pos, 5);
+			}
+    	}
+    	else
+    	{
+    		this.transform.position = new Vector3(this.transform.position.x ,this.transform.position.y , this.transform.position.z + length);
+    	}
+    }
+}
---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+	[SerializeField]
+	private float fadeSecond;
+	[SerializeField]
+	private Image fadeImage;
+
+	[SerializeField]
+	private string[] sceneNames;
+
+	private Dictionary<int, string> sceneNamesIdx;
+	private bool fade;
+	private bool fadeIn;
+	private float fadeSpeed;
+	private int beforScene;
+	private int nowScene;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	private void Start()
+	{
+		fadeSpeed = 1.0f / (fadeSecond * 60.0f);
+	}
+
+	private void Update()
+	{
+
+		// fade
+		if (fade != true)
+		{
+			return;
+		}
+
+		Color color = fadeImage.color;
+
+		if (fadeIn)
+		{
+			color.a -= fadeSpeed;
+
+			if (color.a <= 0.0f)
+			{
+				color.a = 0.0f;
+				fade = false;
+				fadeIn = false;
+			}
+
+		}
+		else
+		{
+			color.a += fadeSpeed;
+
+			if (color.a >= 1.0f)
+			{
+				color.a = 1.0f;
+				fadeIn = true;
+				SceneManager.LoadScene(nowScene);
+			}
+		}
+
+		fadeImage.color = new Color(color.r, color.g, color.b, color.a);
+
+	}
+
+	public void SceneChange(int sceneNumber)
+	{
+		beforScene = nowScene;
+		nowScene = sceneNumber;
+		fade = true;
+		fadeIn = false;
+	}
+
+	public bool isFade()
+	{
+		return fade;
+	}
+
+	public bool isFadeIn()
+	{
+		return fadeIn;
+	}
+}
